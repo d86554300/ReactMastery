7523eafdde1c48328c18dc659bd75a55c87cb028
Leonardo Ribeiro <d86554300@gmail.com>
2025-06-06 14:19:26 -0300

    Created <Form> component to study the useState hook.

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Counter from './Counter/Counter.jsx';
+import Form from './Form/Form';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App>
       <Counter/>
+      <Form/>
     </App>
   </StrictMode>
 )
